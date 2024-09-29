e21ae10e3cee997d14eac15ded423e42c0d716e2
uetak330 <uetak330@gmail.com>
2024-09-29 20:05:27 +0900

    初回コミット

---
+++ Spring04JSP/src/test/java/uetak/Spring04JspApplicationTests.java
@@ -0,0 +1,13 @@
+package uetak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring04JspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
